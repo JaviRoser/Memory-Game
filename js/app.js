ba3c6540cae7e4dc5d8f5dba6f3bbe339406a947
Carlos Sanchez <admroser@gmail.com>
2018-05-28 15:15:07 -0400

    fix:reset button

--- js/app.js
+++ js/app.js
@@ -24,11 +24,25 @@ const cardSymbols = ["diamond",
 /*Selectors*/
 let deck = document.querySelector(".deck");
 let cards = document.querySelector(".deck").children;
-let openCards = []; // Array to hold open Cards
-const numberOfMoves = document.querySelector(".moves");
+let numberOfMoves = document.querySelector(".moves");
+let reset = document.querySelector(".restart");
 
 /*Variables*/
 let moves = 0;
+
+/*Arrays*/
+let openCards = []; // Array to hold open Cards
+
+/*Initialize the game*/
+startGame();
+
+function startGame() {
+	numberOfMoves.textContent = '0';
+	moves = 0;
+	shuffle(cardSymbols);
+	AddCardSymbols();
+	flipCards();
+}
 
 /*
  * Display the cards on the page
@@ -36,17 +50,6 @@ let moves = 0;
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
-
-startGame();
-
-function startGame() {
-	let shuffleCards = shuffle(cardSymbols);
-	numberOfMoves.textContent = '0';
-	AddCardSymbols();
-	moves = 0;
-}
-
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -95,66 +98,77 @@ function AddCardSymbols() {
 /*Get all the cards from the deck*/
 
 
-//Spread Operator
-let deckOfcards = [...cards]; //Contains list of card Symbols
-// /*Flip Cards Function
-// function flipCard(){
-for (let card of deckOfcards) {
-	card.addEventListener('click', function (e) {
-
-		if (!card.classList.contains('open') && !card.classList.contains('show')) {
-			openCards.push(card);
-			card.classList.add('open', 'show');
-			console.log(openCards.length);
-			// clickedCards.push(card);
-			// numberOfMoves.textContent=clickedCards.length;
-
-			if (openCards.length == 2) {
-				//hide
-
-				if (openCards[0].dataset.card === openCards[1].dataset.card) {
-
-					openCards[0].classList.add('match');
-					openCards[1].classList.add('match');
-					openCards[0].classList.remove('open', 'show');
-					openCards[1].classList.remove('open', 'show');
-
-					// openCards[2].classList.remove('open', 'show','match');
-
-					console.log("this is a match");
-					// console.log(openCards[0].dataset.card);
-					// console.log(openCards[1].dataset.card);
-					openCards = [];
-					allMatchedCards();
-				} else {
-					//Mike Wales FEND P3: Memory Game with Mike Wales
-					setTimeout(function () {
-						openCards.forEach(function (card) {
-							card.classList.remove('open', 'show');
-						});
-						openCards = [];
-					}, 1000);
-					moves += 1; //Counting moves
-					numberOfMoves.textContent = moves;
-				}
-
+// /*Flip Cards Function & check if they are a match
+function flipCards() {
+	//Spread Operator
+	let deckOfcards = [...cards]; //Contains list of card Symbols
+	for (let card of deckOfcards) {
+		card.addEventListener('click', function (e) {
+			if (!card.classList.contains('open', 'show')) {
+				openCards.push(card);
+				card.classList.add('open', 'show');
+				matchedCards();
 			}
-
-
-		}
-
-
-		// matchedCards();
-	});
+		});
+	}
 }
 
-// }
 
+function updateNumberofMoves() {
+	numberOfMoves.textContent = moves;
+}
+
+
+function matchedCards() {
+
+
+	if (openCards.length == 2) {
+
+		if (openCards[0].dataset.card === openCards[1].dataset.card) {
+			openCards[0].classList.add('match');
+			openCards[1].classList.add('match');
+			openCards[0].classList.remove('open', 'show');
+			openCards[1].classList.remove('open', 'show');
+			//Prevent user from clicking the matched cards
+			openCards[0].style.pointerEvents = 'none';
+			openCards[1].style.pointerEvents = 'none';
+			openCards = [];
+			allMatchedCards();
+
+		} else {
+			unmatchedCards();
+			moves++; //Counting moves
+			updateNumberofMoves();
+		}
+
+	}
+
+
+}
+
+function unmatchedCards() {
+	//Mike Wales FEND P3: Memory Game with Mike Wales
+
+	openCards[0].classList.add('unmatch');
+	openCards[1].classList.add('unmatch');
+	setTimeout(function () {
+		openCards.forEach(function (card) {
+			// card.classList.add('unmatch');
+			card.classList.remove('open', 'show', 'unmatch');
+
+		});
+		openCards = [];
+
+	}, 1000);
+	// card.classList.remove('unmatch');
+
+}
 
 /*Match two cards(Verify if the two cards that were pushed into openCards Array are a match*/
 
 function allMatchedCards() {
 	let matchCard = deck.getElementsByClassName("match");
+
 	if (matchCard.length == 16) {
 		// Get the modal
 		/*Courtesy of W3School*/
@@ -165,7 +179,6 @@ function allMatchedCards() {
 		// let numberOfMovesContent = document.querySelector("numofMoves");
 		// numberOfMovesContent.textContent = moves;
 		modal.style.display = "block";
-
 
 		// When the user clicks on <span> (x), close the modal
 		span.onclick = function () {
@@ -193,14 +206,8 @@ function allMatchedCards() {
 	Reset Function
 */
 
-const reset = document.querySelector(".restart");
 
 reset.addEventListener('click', function (e) {
-	for (let card of deckOfcards) {
-		card.classList.remove("open", "show");
-		// console.log (card);
-	};
-
-	// startGame();
+	startGame();
 
 });
